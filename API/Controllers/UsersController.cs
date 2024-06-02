4d23816e5d24e37438cc6c7a521acec12c74fcc4
Siddharth Vats <siddharth.vats@cginfinity.com>
2024-06-02 11:25:58 +0530

    Pushing the change

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -21,7 +21,7 @@ public class UsersController : ControllerBase
         return await _context.Users.ToListAsync();
     }
 
-    [HttpGet("{id}")] // api/users/2 
+    [HttpGet("{id}")] // api/users/2  
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
         return await _context.Users.FindAsync(id);
